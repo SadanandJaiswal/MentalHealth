6fb6e28ecb56d85bbc778417a60fc89817197ec8
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2023-07-17 23:38:18 +0530

    login and signup updated

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,11 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import styles from './LoginForm.module.css';
 import { Link } from 'react-router-dom';
+import axios from "axios";
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+
 
 const LoginForm = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = user;
+
+  const navigate = useNavigate();
+
+  const handelLogin = (event) => {
+    event.preventDefault();
+
+	axios.post('/login',{email,password})
+	.then(response=>{
+		console.log('response is ');
+		console.log(response);
+		if(response.data)
+		{
+      axios.get("/me").then(res=>{
+        Cookies.set('userData', JSON.stringify(res.data) ,  { expires: 10*86400*1000 });
+        console.log('user data is')
+        console.log(res.data)
+        if(Cookies.get('userData'))
+        {
+          navigate('/');
+        }
+      })
+      .catch((err)=>{
+        console.log('unable to fetch user having error ',err);
+      })
+		}
+	})
+	.catch((e)=>{
+		console.log('eroor is ',e);
+	})
+	
+  };
+
+  const loginDataChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  useEffect(()=>{
+		console.log(user.email);
+		console.log(user.password);
+	},[user])
+
+
+
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -17,16 +71,23 @@ const LoginForm = () => {
         <div className={styles.LoginForm_shape}></div>
         <div className={styles.LoginForm_shape}></div>
       </div>
-      <form className={styles.LoginForm_form}>
+      <form className={styles.LoginForm_form} encType="multipart/form-data" onSubmit={handelLogin}>
         <h3>Login Here</h3>
-        <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
+        <label htmlFor="username">Email</label>
+        <input 
+        type="text" 
+        placeholder="Email" 
+        onChange={loginDataChange}
+				name="email"
+        id="username" />
         <label htmlFor="password">Password</label>
         <div className={styles.show_password_button}>
           <input
             type={passwordVisible ? 'text' : 'password'}
             id="myInput"
             placeholder="Enter your password"
+            name="password"
+			      onChange={loginDataChange}
           />
           <FontAwesomeIcon
             icon={passwordVisible ? faEyeSlash : faEye}
--- frontend/src/pages/Signup/index.jsx
+++ frontend/src/pages/Signup/index.jsx
@@ -1,11 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import styles from './LoginForm.module.css';
 import { Link } from 'react-router-dom';
+import axios from "axios";
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+
 
 const LoginForm = () => {
+  const [user, setUser] = useState({
+		name:"",
+		email:"",
+		password:""
+	});
+
   const [passwordVisible, setPasswordVisible] = useState(false);
+
+	const navigate = useNavigate();
+
+  const submitForm = (event)=>{
+		event.preventDefault();
+		const name = event.target.name.value;
+		const email = event.target.email.value;
+		const password = event.target.password.value;
+
+		axios.post("/register",{name,email,password})
+		.then(response=>{
+			console.log('response is ');
+			console.log(response);
+			if(response.data)
+			{
+				axios.get("/me").then(res=>{
+					Cookies.set('userData', JSON.stringify(res.data) ,  { expires: 10*86400*1000 });
+					if(Cookies.get('userData'))
+					{
+					  navigate('/');
+					}
+				})
+				.catch((err)=>{
+					console.log('unable to fetch user having error ',err);
+				})
+			}
+		})
+		.catch((e)=>{
+			console.log('eroor is ',e);
+		})
+	}
+
+
+  const registerDataChange = (e) =>{
+		setUser({ ...user, [e.target.name]: e.target.value });
+	}
+
+	useEffect(()=>{
+		console.log(user.name);
+		console.log(user.email);
+		console.log(user.password);
+	},[user]);
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -17,17 +69,21 @@ const LoginForm = () => {
         <div className={styles.LoginForm_shape}></div>
         <div className={styles.LoginForm_shape}></div>
       </div>
-      <form className={styles.LoginForm_form}>
+      <form className={styles.LoginForm_form} encType="multipart/form-data" onSubmit={submitForm}>
         <h3>SignUp</h3>
         <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
+        <input
+        onChange={registerDataChange} name="name" type="text" placeholder="Email or Phone" id="username" />
         <label htmlFor="email">Email</label>
-        <input type="text" placeholder="Email" id="email" />
+        <input
+        onChange={registerDataChange} name="email" type="text" placeholder="Email" id="email" />
         <label htmlFor="password">Password</label>
         <div className={styles.show_password_button}>
           <input
+          onChange={registerDataChange}
             type={passwordVisible ? 'text' : 'password'}
             id="myInput"
+            name="password"
             placeholder="Enter your password"
           />
           <FontAwesomeIcon
