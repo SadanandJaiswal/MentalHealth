a8716ae8640e07873787974f012c5e406d86fb56
JyotsnaTelgote <jyotsnajiet@gmail.com>
2023-07-17 19:12:00 +0530

    QuotesSection Added

--- frontend/src/pages/Quotes.js
+++ frontend/src/pages/Quotes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Quotesitem from './Quotesitem'
+import './quotes.css'
 
 const Quotes = () => {
     const articles=[
         {
           "id":"1",
           "author": "Grace Kay",  
-          "urlToImage": "https://i.postimg.cc/vBSWHpxw/s2.jpg",
+          "urlToImage": "https://i.postimg.cc/G3sZ04gs/i1.jpg",
           "publishedAt": "2023-02-22T15:05:33Z",
           "content": null,
           "category":"stress"
@@ -14,7 +15,7 @@ const Quotes = () => {
         {
           "id":"2",
           "author": "UNKNOWN",  
-          "urlToImage": "https://i.postimg.cc/DyTmBrbY/u.jpg",
+          "urlToImage": "https://i.postimg.cc/Xq1YRpYJ/q11.jpg",
           "publishedAt": "2023-02-22T15:05:33Z",
           "content": null,
           "category":"stress"
@@ -22,7 +23,7 @@ const Quotes = () => {
         {
           "id":"3",
           "author": "Unknown",  
-          "urlToImage": "https://i.postimg.cc/90s96L8s/iop.jpg",
+          "urlToImage": "https://i.postimg.cc/tgHysh1P/i1.jpg",
           "publishedAt": "2023-02-22T15:05:33Z",
           "content": null,
           "category":"stress"
@@ -30,7 +31,7 @@ const Quotes = () => {
         {
           "id":"4",
           "author": "Zig Zay",  
-          "urlToImage": "https://i.postimg.cc/cJx8LQcb/lu4.jpg",
+          "urlToImage": "https://i.postimg.cc/wjj40MSP/i1.jpg",
           "publishedAt": "2023-02-22T15:05:33Z",
           "content": null,
           "category":"stress"
@@ -46,7 +47,7 @@ const Quotes = () => {
           {
             "id":"6",
             "author": "Anonyms",  
-            "urlToImage": "https://i.postimg.cc/v8nWz1y2/p.jpg",
+            "urlToImage": "https://i.postimg.cc/j50cf4PH/i1.jpg",
             "publishedAt": "2023-02-22T15:05:33Z",
             "content": null,
             "category":"stress"
@@ -54,7 +55,7 @@ const Quotes = () => {
           {
             "id":"7",
             "author": "Anonyms",  
-            "urlToImage": "https://i.postimg.cc/brBDQZH8/io.png",
+            "urlToImage": "https://i.postimg.cc/v8nWz1y2/p.jpg",
             "publishedAt": "2023-02-22T15:05:33Z",
             "content": null,
             "category":"stress"
@@ -62,7 +63,7 @@ const Quotes = () => {
           {
             "id":"8",
             "author": "Anonyms",  
-            "urlToImage": "https://i.postimg.cc/kML7t0kW/think.jpg",
+            "urlToImage": "https://i.postimg.cc/7ZCK80bv/qw7.jpg",
             "publishedAt": "2023-02-22T15:05:33Z",
             "content": null,
             "category":"stress"
@@ -70,7 +71,31 @@ const Quotes = () => {
           {
             "id":"9",
             "author": "Anonyms",  
-            "urlToImage": "https://i.postimg.cc/kML7t0kW/think.jpg",
+            "urlToImage": "https://i.postimg.cc/v8nWz1y2/p.jpg",
+            "publishedAt": "2023-02-22T15:05:33Z",
+            "content": null,
+            "category":"stress"
+          },
+          {
+            "id":"10",
+            "author": "Anonyms",  
+            "urlToImage": "https://i.postimg.cc/wxmxvbP3/qw3.jpg",
+            "publishedAt": "2023-02-22T15:05:33Z",
+            "content": null,
+            "category":"stress"
+          },
+          {
+            "id":"11",
+            "author": "Anonyms",  
+            "urlToImage": "https://i.postimg.cc/0jTqGxBr/qw1.jpg",
+            "publishedAt": "2023-02-22T15:05:33Z",
+            "content": null,
+            "category":"stress"
+          },
+          {
+            "id":"12",
+            "author": "Anonyms",  
+            "urlToImage": "https://i.postimg.cc/WbYZvyJz/qw.jpg",
             "publishedAt": "2023-02-22T15:05:33Z",
             "content": null,
             "category":"stress"
@@ -78,28 +103,25 @@ const Quotes = () => {
         
       ];
   return (
-        <>
-        <div className='my-3'>
-         <h1>Quotes!!</h1>
-         <div className="row">
-         {articles.map((element) => {
-            return (
-                <div className='col-md-4' quoteid={element.id}>
-                    {/* {window.confirm(element.)} */}
-
-                <Quotesitem
-                    imageUrl={element.urlToImage}
-                    author={element.author}
-                    category={element.category}
-                    date={element.publishedAt}
-                />
-                </div>
-            );
-            })}
-         </div>
-        </div>
-        </>
+    <>
+    <div className='agko'>
+      <h1 style={{color:"black"}}>Quotes!!</h1>
+      <div className='quote-container'>
+        {articles.map((element) => (
+          <div className='quote-item' key={element.id} >
+            <Quotesitem
+              imageUrl={element.urlToImage}
+              author={element.author}
+              category={element.category}
+              date={element.publishedAt}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  </>
        )
 }
 
 export default Quotes
+
