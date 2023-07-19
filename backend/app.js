55437144cbf412492bb2037ddc2d11cc227218b4
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2023-07-20 01:26:02 +0530

    some more changes to deploy

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cors());
 // app.use(cors({ origin:'https://jeevanbandhu.netlify.app', credentials:true }));
-app.use(cors({origin:"*", credentials:true}));
+// app.use(cors({origin:"*", credentials:true}));
+
+let origin ;
+app.use((req,res,next)=>{
+    origin = req.headers.origin;
+    console.log('origin is ',origin);
+})
+
+// const allowedOrigins = ['http://localhost', 'https://jeevanbandhu.netlify.app'];
+
+if(origin == "localhost")
+{
+    app.use(cors({
+        origin: 'http://localhost',
+        credentials:true
+    }))
+}
+else{
+    app.use(cors({
+        origin: 'https://jeevanbandhu.netlify.app',
+        credentials:true
+    }))
+}
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+
+// Enable CORS for all routes
+// app.use(cors(corsOptions));
 
 
 const routes = require('./routes/routes')
