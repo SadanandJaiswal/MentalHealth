f04deed4c919fe1d612db62464bcb7d7eaffbe10
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2023-07-20 10:57:07 +0530

    secure:true for cookie option

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -24,14 +24,15 @@ const sendToken = (user, statuscode, res, isLocalhost) => {
     const token = user.getJWTToken();
   
     // Determine the appropriate domain based on the environment
-    const domain = isLocalhost ? "localhost" : "jeevanbandhu.netlify.app";
+    // const domain = isLocalhost ? "localhost" : "jeevanbandhu.netlify.app";
   
     // Options for cookie
     const options = {
       expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
       httpOnly: false,
       domain: "*", // Set the domain based on the environment
-      secure: !isLocalhost, // Set to true for production (HTTPS)
+      // secure: !isLocalhost, // Set to true for production (HTTPS)
+      secure: true,
       sameSite: "None", // You can keep this as "None" for cross-site access with HTTPS
     };
   
